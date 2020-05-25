d2770b9cd36b75ce61b71f565791767f35550f53
prasunbhunia <prasunbhunia99@gmail.com>
2020-05-25 16:12:16 +0530

    Updates

---
+++ precache-manifest.969feb1c1a61ae885af1a877bce9d4d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed2e0293bb2f78e4af366a8d1d6eadb",
+    "url": "/star-game/index.html"
+  },
+  {
+    "revision": "8aa5f14eee2e2039207e",
+    "url": "/star-game/static/css/main.09294779.chunk.css"
+  },
+  {
+    "revision": "3b2302ff4a1e53a8ad65",
+    "url": "/star-game/static/js/2.c80dc526.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-game/static/js/2.c80dc526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa5f14eee2e2039207e",
+    "url": "/star-game/static/js/main.0bdda845.chunk.js"
+  },
+  {
+    "revision": "56fb5f9ee1e6e9fe5709",
+    "url": "/star-game/static/js/runtime-main.270aece1.js"
+  }
+]);
